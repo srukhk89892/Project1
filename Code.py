3963d84d4e2949417dbf7c953c37c0004cf282d6
srukhk89892 <srukhk.89892@gmail.com>
2026-03-17 18:15:24 +0530

    Added a function to calculate Average

--- Code.py
+++ Code.py
@@ -6,3 +6,17 @@ def printTable(n):
 
 n = int(input ("Enter a number: "));
 printTable(n);
+
+
+# Write a python function to calculate averag 3 User inputs
+
+
+def calculateAverage(a,b,c):
+    average = (a+b+c)/3
+    return average;
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+
+print("The average of the three numbers are :", calculateAverage(a,b,c) )
